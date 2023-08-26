d5c61a50f603f6a41e1c6e41f3630234b63d2ac9
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2023-08-26 19:35:34 +0530

    Used logging

--- image_ana.py
+++ image_ana.py
@@ -1,11 +1,13 @@
 from cv2 import imread, cvtColor, inRange
 from processing import makeExcel, os, np, pd
 from multiprocessing import Pool
+from logging import basicConfig, INFO, WARNING, CRITICAL, ERROR, DEBUG, info, warning, error, critical, debug 
 
 y = "Concentration"
 x = "Intensity"
 data = pd.DataFrame(columns=[y, x])
 VAL_RANGES = [210, 175,170, 160,140,80,55, 40]
+basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=INFO)
 
 def processFolder(folder_path):
     subfolder_paths =  [os.path.join(folder_path, path) for path in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, path))]
@@ -19,14 +21,13 @@ def singleFolder(subfolder):
         print(concentration)
         images = [os.path.join(subfolder, image_path) for image_path in os.listdir(subfolder) if image_path.endswith((".jpg", ".png", ".jpeg"))]
         for image_path in images:
-            print("\t----------------------------------------------------------------------------------------")
-            print(f"\t working with {os.path.split(image_path)[-1]}")
+            debug(f"\t working with {os.path.split(image_path)[-1]}")
             image = imread(image_path)
             mean = getMean(image, concentration)
-            print(f"\t\t Intensity: {mean}")
+            info(f"\t\t Intensity: {mean}")
             data.loc[len(data)] = [concentration, mean]
     except Exception as e:
-        print(f"Error, issue is {e}")
+        error(f"Error, issue is {e}")
 
 def getMean(image,concentration):
     mean = 0
@@ -34,33 +35,33 @@ def getMean(image,concentration):
     print("\t\t\t working... line 32")
     for lightness in VAL_RANGES:
         mean, p_length = calculateMean(image, hsv_img, lightness)
-        print("\t\t\t working... line 35")
+        debug("\t\t\t working... line 35")
         if p_length < 10000:
-            print("\t\t\t working... line 37")
+            debug("\t\t\t working... line 37")
             continue 
         elif len(data) > 2:
             prev_mean = data[x].iloc[-1]
             req_range = 7 if data[y].iloc[-1] == concentration else 17
-            print("\t\t\t working... line 42")
+            debug("\t\t\t working... line 42")
             t_mean = mean
             t_lightness = lightness
             while abs(t_mean-prev_mean) > req_range:
                 if t_mean > prev_mean:
                     t_lightness = VAL_RANGES[VAL_RANGES.index(t_lightness)+1]
 
-                    print("\t\t\t working... line 47")
+                    debug("\t\t\t working... line 47")
                 else:
                     t_lightness = VAL_RANGES[VAL_RANGES.index(t_lightness)-1]
-                    print("\t\t\t working... line 50")
+                    debug("\t\t\t working... line 50")
                 t_mean, _ = calculateMean(image, hsv_img, t_lightness)
-                print("\t\t\t working... line 53 t_mean is ", t_mean, " at lightness ", t_lightness)
+                info("\t\t\t working... line 53 t_mean is ", t_mean, " at lightness ", t_lightness)
             mean = t_mean
-            print("\t\t\t working... line 56")
+            debug("\t\t\t working... line 56")
             lightness = t_lightness
-            print("\t\t\t working... line 58")
+            debug("\t\t\t working... line 58")
             return mean
         else:
-            print("\t\t\t working... line 61")
+            debug("\t\t\t working... line 61")
             break
     return mean
 
@@ -68,12 +69,12 @@ def getMean(image,concentration):
 def calculateMean(image, hsv_image, lightness):
     min_range, max_range = [np.array([110,170, lightness]), np.array([120, 255,255])]
     mask = inRange(hsv_image, min_range, max_range)
-    print("\t\t\t working... line 69")
+    debug("\t\t\t working... line 69")
     required_pixels = image[mask==255]
-    print("\t\t\t working... line 70; size of required pixels is ", required_pixels.shape[0], " at lightness ", lightness)
+    info("\t\t\t working... line 70; size of required pixels is ", required_pixels.shape[0], " at lightness ", lightness)
     mean =0
     if required_pixels.shape[0] > 0:
-        print("\t\t\t working... line 72; calculated mean is ")
+        debug("\t\t\t working... line 72; calculated mean is ")
         mean = np.mean(required_pixels)
-        print(f"\t\t\t\t {mean} at line 75")
+        info(f"\t\t\t\t {mean} at line 75")
     return mean, required_pixels.shape[0]
