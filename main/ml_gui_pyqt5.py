855a5b6c8c91b73c1912d7d975c9b7c788c7075f
Shashwat Goel <itshashgoel@gmail.com>
2023-11-30 13:06:11 +0530

    Brought Real-Time first

--- main/ml_gui_pyqt5.py
+++ main/ml_gui_pyqt5.py
@@ -144,10 +144,10 @@ class MainWindow(QMainWindow):
         self.prediction_tab = QWidget()
         self.about_us_tab = QWidget()
 
+        self.tab_widget.addTab(self.real_time_tab, "Real Time Analysis")
         self.tab_widget.addTab(self.image_analysis_tab, "Image Analysis")
         self.tab_widget.addTab(self.data_analysis_tab, "Data Analysis")
         self.tab_widget.addTab(self.prediction_tab, "Prediction")
-        self.tab_widget.addTab(self.real_time_tab, "Real Time Analysis")
         self.tab_widget.addTab(self.about_us_tab, "About")
 
         # Layout for Image Analysis tab
